cfedb5172382d0bd3ecab13c366f1ef1579f6302
KangMinseok <qbq5000@gmail.com>
2020-08-20 07:13:43 +0900

    Add TokenManager & Modify login response model

---
+++ D-Book/Managers/TokenManager.swift
@@ -0,0 +1,35 @@
+//
+//  AuthManager.swift
+//  D-Book
+//
+//  Created by 강민석 on 2020/08/19.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import KeychainAccess
+
+class TokenManager {
+    
+    static let shared = TokenManager()
+    
+    private init() {}
+    
+    fileprivate let tokenKey = "TokenKey"
+    fileprivate let keychain = Keychain(service: "com.tistory.axe-num1.D-Book")
+    
+    var token: String? {
+        get {
+            guard let token = keychain[tokenKey] else { return nil }
+            return token
+        }
+        set {
+            if let token = newValue {
+                keychain[tokenKey] = token
+            }
+        }
+    }
+    
+}
--- D-Book/Network/Login/Response/PostLogin.swift
+++ D-Book/Network/Login/Response/PostLogin.swift
@@ -8,44 +8,16 @@
 
 import Foundation
 
-struct PostLogin {
+struct PostLogin: ResponseProtocol {
+    var status: Int?
+    var message: String?
+    var email: String?
+    var accessToken: String?
     
-    class Response : ResponseProtocol {
-        var status: Int?
-        var message: String?
-        var data: Data?
-        
-        enum ResponseKey : String, CodingKey{
-            case status
-            case message
-            case data
-        }
-        
-        required convenience init(from decoder: Decoder) throws {
-            self.init()
-            
-            let container = try decoder.container(keyedBy: ResponseKey.self)
-            
-            self.status = try container.decode(Int.self, forKey: .status)
-            self.message = try container.decodeIfPresent(String.self, forKey: .message)
-            self.data = try container.decodeIfPresent(Data.self, forKey: .data)
-            
-        }
-    }
-    
-    class Data : Decodable{
-        var token: String?
-        
-        enum DataKey : String, CodingKey{
-            case token
-        }
-        
-        required convenience init(from decoder: Decoder) throws {
-            self.init()
-            
-            let container = try decoder.container(keyedBy: DataKey.self)
-            
-            self.token = try container.decodeIfPresent(String.self, forKey: .token)
-        }
+    enum CodingKeys : String, CodingKey {
+        case status
+        case message
+        case email
+        case accessToken = "token"
     }
 }
