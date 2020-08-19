d023c49c2fcce12b5f8fc7d299c2d0f8dc2540b5
KangMinseok <qbq5000@gmail.com>
2020-08-20 07:11:52 +0900

    Implement Login / Main transition

--- D-Book/Application/AppDelegate.swift
+++ D-Book/Application/AppDelegate.swift
@@ -10,14 +10,51 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let loginStoryBoard = UIStoryboard(name: "Login", bundle: nil)
+        
+        if UserDefaults.standard.string(forKey: "username") != nil {
+            let mainTabBarController = mainStoryboard.instantiateViewController(identifier: "MainTabBarController")
+            window?.rootViewController = mainTabBarController
+        } else {
+            let loginViewController = loginStoryBoard.instantiateViewController(identifier: "LoginViewController")
+            window?.rootViewController = loginViewController
+        }
+        
         return true
     }
-
-
+    
+    func changeRootViewController(_ vc: UIViewController, animated: Bool = true) {
+        guard let window = self.window else {
+            return
+        }
+        
+        window.rootViewController = vc
+        
+        UIView.transition(with: window,
+                          duration: 0.5,
+                          options: [.transitionFlipFromLeft],
+                          animations: nil,
+                          completion: nil)
+    }
+    
+    /* Go to main
+     let storyboard = UIStoryboard(name: "Main", bundle: nil)
+     let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
+     (UIApplication.shared.delegate as? AppDelegate)?.changeRootViewController(mainTabBarController)
+     */
+    
+    /* Go to login
+     let storyboard = UIStoryboard(name: "Login", bundle: nil)
+     let loginViewController = storyboard.instantiateViewController(identifier: "LoginViewController")
+     (UIApplication.shared.delegate as? AppDelegate)?.changeRootViewController(loginViewController)
+     */
+    
+    
 }
 
