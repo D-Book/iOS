d92230637961dc793f749ad66eecd3c0732e82b8
KangMinseok <qbq5000@gmail.com>
2020-08-20 07:10:17 +0900

    Login Scene modify

---
+++ D-Book/Scenes/Login/LoginViewController.swift
@@ -0,0 +1,70 @@
+//
+//  LoginViewController.swift
+//  D-Book
+//
+//  Created by 강민석 on 2020/07/24.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    let viewModel = LoginViewModel()
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bindViewModel()
+    }
+    
+}
+
+extension LoginViewController {
+    
+    func bindViewModel() {
+        let input = LoginViewModel.Input(loginTrigger: loginButton.rx.tap.asObservable())
+        let output = viewModel.transform(input: input)
+        
+        emailField.rx.text.orEmpty
+            .bind(to: viewModel.id)
+            .disposed(by: disposeBag)
+        
+        passwordField.rx.text.orEmpty
+            .bind(to: viewModel.pw)
+            .disposed(by: disposeBag)
+        
+        output.loginButtonEnabled
+            .drive(onNext: { [weak self] isEnabled in
+                self?.loginButton.isEnabled = isEnabled
+            }).disposed(by: disposeBag)
+    }
+    
+    func configureCallback() {
+        
+        viewModel.isLoading
+            .bind { [weak self] isLoading in
+                if isLoading {
+                    self?.startIndicatingActivity()
+                }
+            }.disposed(by: disposeBag)
+        
+        viewModel.isSuccess
+            .bind { isSuccess in
+                self.stopIndicatingActivity()
+                
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
+                (UIApplication.shared.delegate as? AppDelegate)?.changeRootViewController(mainTabBarController)
+            }.disposed(by: disposeBag)
+        
+    }
+    
+}
---
+++ D-Book/Scenes/Login/LoginViewModel.swift
@@ -0,0 +1,73 @@
+//
+//  LoginViewModel.swift
+//  D-Book
+//
+//  Created by 강민석 on 2020/07/24.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class LoginViewModel: ViewModelType {
+    
+    struct Input {
+        let loginTrigger: Observable<Void>
+    }
+    
+    struct Output {
+        let loginButtonEnabled: Driver<Bool>
+    }
+    
+    let disposeBag = DisposeBag()
+    let isSuccess = BehaviorRelay(value: false)
+    let isLoading = BehaviorRelay(value: false)
+    let errorMsg = BehaviorRelay(value: "")
+    
+    let networkClient = NetworkClient()
+    var loginRequest = PostLoginRequest()
+    
+    let id = BehaviorRelay(value: "")
+    let pw = BehaviorRelay(value: "")
+
+    func transform(input: Input) -> Output {
+        
+        loginRequest.id = self.id.value
+        loginRequest.pw = self.pw.value
+        
+        let response = input.loginTrigger
+            .flatMapLatest {
+                self.networkClient.postRequest(PostLogin.self, endpoint: "", param: self.loginRequest)
+            }
+            
+        response
+            .subscribe(
+                onNext: { response in
+//                    KeychainService.username = self.emailIdViewModel.data.value
+//                    KeychainService.password =  self.passwordViewModel.data.value
+//
+//                    AuthController.signIn(token: response.data!.token!, refreshToken: response.data!.refreshToken!)
+                    
+                    TokenManager.shared.token = response.accessToken
+                    
+                    response.email
+                    
+                    self.isLoading.accept(false)
+                    self.isSuccess.accept(true)
+                },
+                onError: { error in
+                    self.isLoading.accept(false)
+                    self.errorMsg.accept(error.localizedDescription)
+                }
+            ).disposed(by : disposeBag)
+        
+        let loginButtonEnabled = BehaviorRelay.combineLatest(id, pw, self.isLoading.asObservable()) {
+            return !$0.isEmpty && !$1.isEmpty && !$2
+        }.asDriver(onErrorJustReturn: false)
+            
+        return Output(loginButtonEnabled: loginButtonEnabled)
+    }
+    
+    
+}
