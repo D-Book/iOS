63ed94b1da6036bdd696d212a95add92474b3ddd
KangMinseok <qbq5000@gmail.com>
2020-11-08 14:45:36 +0900

    FEATURE: Config파일 추가

---
+++ D-Book/Configs/Configs.swift
@@ -0,0 +1,35 @@
+//
+//  Configs.swift
+//  D-Book
+//
+//  Created by 강민석 on 2020/11/07.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import UIKit
+
+struct Configs {
+
+    struct App {
+        static let bundleIdentifier = "com.tistory.axe-num1.D-Book"
+    }
+
+    // 외부 IP:
+    // 내부 IP: http://10.80.162.210:8080
+    struct Network {
+        static let baseURL = URL(string: "http://10.80.162.210:8080")!
+    }
+
+    struct BaseDimensions {
+
+    }
+
+    struct BaseColor {
+        
+    }
+
+    struct Path {
+
+    }
+
+}
