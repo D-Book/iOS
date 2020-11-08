111b86997da98bfbb80d47346e6613a34487db53
KangMinseok <qbq5000@gmail.com>
2020-11-08 19:39:37 +0900

    [FEATURE] 프로필 생성 코드 추가

--- D-Book/Modules/CreateProfile/CreateProfileViewController.swift
+++ D-Book/Modules/CreateProfile/CreateProfileViewController.swift
@@ -10,20 +10,80 @@ import UIKit
 import Reusable
 import RxSwift
 import RxCocoa
+import RxGesture
 
 class CreateProfileViewController: BaseViewController, StoryboardSceneBased {
     
     static let sceneStoryboard = R.storyboard.createProfile()
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var completeButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setProfileImageView()
+        bindProfileImage()
     }
     
     override func bindViewModel() {
         super.bindViewModel()
         
-    }
+        guard let viewModel = viewModel as? CreateProfileViewModel else { fatalError("ViewModel: \(self.viewModel!) Casting Error") }
+        
+        let nameControlEvents: Observable<Bool> = Observable.merge([
+            nameTextField.rx.controlEvent(.editingDidBegin).map { true },
+            nameTextField.rx.controlEvent(.editingDidEnd).map { false }
+        ])
+        
+        let input = CreateProfileViewModel.Input(completeSelection: completeButton.rx.tap.asObservable(),
+                                                 nameEvents: nameControlEvents)
+        let output = viewModel.transform(input: input)
+        
+        nameTextField.rx.text.orEmpty
+            .bind(to: viewModel.username)
+            .disposed(by: rx.disposeBag)
 
+        output.completeButtonEnabled
+            .drive(completeButton.rx.isEnabled)
+            .disposed(by: rx.disposeBag)
+    }
 }
+
+// MARK: - BindImagePicker
+extension CreateProfileViewController {
+    func bindProfileImage() {
+        guard let viewModel = viewModel as? CreateProfileViewModel else { fatalError("FatalError!") }
+
+        let imagePicker = imagePickerScene(
+            on: self,
+            modalPresentationStyle: .formSheet,
+            modalTransitionStyle: .none
+        )
+
+        profileImageView.rx.tapGesture()
+            .when(.recognized)
+            .flatMapLatest { _ in Observable.create(imagePicker) }
+            .compactMap { $0[.originalImage] as? UIImage }
+            .bind { image in
+                self.profileImageView.image = image
+                viewModel.profileImage.accept(image)
+            }
+            .disposed(by: rx.disposeBag)
+    }
+}
+
+// MARK: - Configure UI
+extension CreateProfileViewController {
+    func setProfileImageView() {
+        profileImageView.roundCorners(.allCorners, radius: 0.5 * profileImageView.frame.width)
+        profileImageView.layer.borderWidth = 2
+        profileImageView.layer.borderColor = UIColor.lightGray.cgColor
+        profileImageView.clipsToBounds = true
+
+        self.profileImageView.image = R.image.profileImage()
+    }
+}
+
--- D-Book/Modules/CreateProfile/CreateProfileViewModel.swift
+++ D-Book/Modules/CreateProfile/CreateProfileViewModel.swift
@@ -11,18 +11,78 @@ import RxCocoa
 
 class CreateProfileViewModel: BaseViewModel {
     
+    let email: String
+    let password: String
+    
+    let profileImage = BehaviorRelay(value: UIImage())
+    let username = BehaviorRelay(value: "")
+    
+    init(email: String, password: String) {
+        self.email = email
+        self.password = password
+    }
+    
     // MARK: - Struct
     struct Input {
-        
+        let completeSelection: Observable<Void>
+        let nameEvents: Observable<Bool>
     }
 
     struct Output {
-        
+        let completeButtonEnabled: Driver<Bool>
     }
     
+    func validateName(_ string: String) -> Bool {
+        return string.range(of: "^[가-힣]{2,4}+$", options: .regularExpression) != nil
+    }
+}
+
+extension CreateProfileViewModel {
     func transform(input: Input) -> Output {
+        let buttonStatus = BehaviorRelay(value: false)
         
-        return Output()
+        input.completeSelection.flatMapLatest { [weak self] () -> Observable<SignInResponse> in
+            guard let self = self else  { return Observable.just(SignInResponse()) }
+            
+            let username = self.username.value
+            let profileImage = self.profileImage.value
+            let request = self.service.signUp(username: username,
+                                              email: self.email,
+                                              password: self.password,
+                                              profileImage: profileImage)
+            return request
+                .trackActivity(self.loading)
+        }.subscribe(onNext: { (signInResponse) in
+            if let user = signInResponse.object?.user, let token = signInResponse.token {
+                DatabaseManager.shared.saveUser(user)
+                TokenManager.shared.token = token
+            }
+            loggedIn.accept(true)
+        }, onError: { (error) in
+            loggedIn.accept(false)
+            self.error.onNext(error as! ErrorResponse)
+        }).disposed(by: rx.disposeBag)
+        
+        // MARK: - Valid Check
+        let isNameValid = username
+            .throttle(RxTimeInterval.milliseconds(100), scheduler: MainScheduler.instance)
+            .map { [weak self] text -> Bool in
+                guard let isValid = self?.validateName(text) else { return false }
+                return !text.isEmpty && isValid
+        }.distinctUntilChanged()
+        
+        // MARK: - Validation Observable
+        Observable.combineLatest(isNameValid, input.nameEvents)
+            .subscribe(onNext: { (isValid: Bool, editingDidEnd: Bool) in
+                if isValid {
+                    buttonStatus.accept(true)
+                } else if editingDidEnd {
+                    buttonStatus.accept(false)
+                } else {
+                    buttonStatus.accept(false)
+                }
+            }).disposed(by: rx.disposeBag)
+        
+        return Output(completeButtonEnabled: buttonStatus.asDriver())
     }
-    
 }
