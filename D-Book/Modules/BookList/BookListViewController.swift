bc0a063cd00399b1af5cdde584654067afc8dd76
KangMinseok <qbq5000@gmail.com>
2020-11-08 15:00:43 +0900

    FEATURE: 책 목록 추가

---
+++ D-Book/Modules/BookList/BookListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  BookListViewController.swift
+//  D-Book
+//
+//  Created by 강민석 on 2020/11/07.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import UIKit
+import Reusable
+import RxSwift
+import RxCocoa
+
+class BookListViewController: BaseViewController, StoryboardSceneBased {
+    
+    static let sceneStoryboard = R.storyboard.bookList()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func bindViewModel() {
+        super.bindViewModel()
+        
+    }
+}
---
+++ D-Book/Modules/BookList/BookListViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  BookListViewModel.swift
+//  D-Book
+//
+//  Created by 강민석 on 2020/11/07.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+class BookListViewModel: BaseViewModel {
+    // MARK: - Struct
+    struct Input {
+        
+    }
+
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        
+        return Output()
+    }
+}
